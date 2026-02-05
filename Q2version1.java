b5b64626bb07b296108814f67decb4d5f5187483
Kevinandam <kabrown001@st.ug.edu.gh>
2026-02-05 14:45:52 +0000

    Add files via upload

---
+++ Q2version1.java
@@ -0,0 +1,6 @@
+public  class Q2version1{
+    public static  double calculateClassAverage(double score1, double score2,){
+        double average =  (score1 +score2) /2;
+        return average;
+    }
+}
